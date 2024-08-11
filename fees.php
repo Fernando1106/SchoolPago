538482c9828c73bc40e53711f3f899ded92deb8b
Fernando1106 <106330355+Fernando1106@users.noreply.github.com>
2024-08-11 11:22:59 +0200

    Update fees.php

--- fees.php
+++ fees.php
@@ -357,7 +357,7 @@ display:none;
     <!-- /. WRAPPER  -->
 
     <div id="footer-sec">
-    Para más desarrollos gratuitos, accede a <a href="https://www.configuroweb.com/" target="_blank">ConfiguroWeb</a>
+    ... <a href="" target="_blank">FSB</a>
     </div>
    
   
