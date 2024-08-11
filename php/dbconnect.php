879f73c6c00790fdecaad66e9affcf9cf7099925
Fernando1106 <106330355+Fernando1106@users.noreply.github.com>
2024-08-11 11:20:53 +0200

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -12,7 +12,7 @@ DEFINE ('DB_PSWD', '');
 DEFINE ('DB_HOST', 'localhost'); 
 DEFINE ('DB_NAME', 'paysystem'); 
 
-date_default_timezone_set('America/Bogota'); 
+date_default_timezone_set('America/Lima'); 
 $conn =  new mysqli(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
 if($conn->connect_error)
 die("Failed to connect database ".$conn->connect_error );
